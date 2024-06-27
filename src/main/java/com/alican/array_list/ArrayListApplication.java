2a2c2da5cccae660fb00127a6e4dfad4d2381a13
KarakusAlican <tr.alikarakus2008@gmail.com>
2024-06-27 22:43:34 +0300

    bitti

--- src/main/java/com/alican/array_list/ArrayListApplication.java
+++ src/main/java/com/alican/array_list/ArrayListApplication.java
@@ -9,16 +9,24 @@ public class ArrayListApplication {
 
 	public static void main(String[] args) {
 
+		int sayi_1=5;
+		int sayi_2=9;
+		int sayi_3=7;
+		int sayi_4=15;
+
+
 		SpringApplication.run(ArrayListApplication.class, args);
 		ArrayList<Integer>degerler =new ArrayList<Integer>();
-		degerler.add(1);
-		degerler.add(2);
-		degerler.add(6);
-		degerler.add(48);
-		Collections.sort(degerler);
+		degerler.add(sayi_1);
+		degerler.add(sayi_2);
+		degerler.add(sayi_3);
+		degerler.add(sayi_4);
+		System.out.println(degerler);
 
-		for (int i : degerler) {
-			System.out.println(i);
+
+
+
+
 
 		}
 
